9d827fc7b766b87cfea97cbc8df82dacf8f5f5c2
Debasish Barai <debasishbaraiju@gmail.com>
2025-03-09 23:05:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -178,7 +178,7 @@ export default function Home() {
         let binary = '';
 
         for (let i=0; i< audioData.length; i++) {
-          binary += String.fromCharCode(bytes[i]);
+          binary += String.fromCharCode(audioData[i]);
         }
 
         return btoa(binary)
